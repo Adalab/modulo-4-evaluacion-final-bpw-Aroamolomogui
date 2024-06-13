fbcf7765273368fc2b2e8c1918a40020a0db9339
Aroa Pavon <aroapavongarcia@gmail.com>
2024-06-13 08:03:55 +0200

    not add conection with DB

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const exprexx= require("express");
+const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2/promise");
 
@@ -10,7 +10,21 @@ server.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
-application.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log(`server running in port : http://localhost:${PORT}`);
 
 });
+
+async function getConnection () {
+    const conex = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user:process.env.DB_USER,
+        password:process.env.DB_PASSWORD,
+        database:process.env.DB_NAME,
+    });
+
+    await conex.connect();
+    console.log('conexion con la BD' + conex.threadId);
+    return conex;
+}
+getConnection();
