c1da1000baf9249248bc7d56980674582847cc27
Aroa Pavon <aroapavongarcia@gmail.com>
2024-06-13 11:20:37 +0200

    endpoints get and get to id

--- bd/bdmoon.sql
+++ bd/bdmoon.sql
@@ -7,3 +7,13 @@ sign varchar (20) not null,
 descripcion text not null,
 afectacion text not null 
 );
+
+INSERT INTO moon (sign, descripcion, afectacion)
+
+VALUES ('Aries', ' son emocionalmente directas y apasionadas. No tienen miedo de mostrar sus sentimientos y pueden ser bastante francas al expresar lo que piensan. Son impulsivas en sus reacciones emocionales y tienden a enfrentar los desafíos con valentía y determinación', '1. comprueba en que signos esta la luna en regencia. 2. verifica como le afecta a tu luna natal. 3. cuida de tus emociones y combina ambas lunas esos dos ias y medio para poder darte de la mejor manera lo que necesitas');
+INSERT INTO moon (sign, descripcion, afectacion)
+
+VALUES ('Tauro', 'son emocionalmente estables y seguros. Tienden a ser leales y confiables en sus relaciones, pero a veces pueden tener dificultades para expresar sus emociones o abrirse completamente a los demás.', '1. comprueba en que signos esta la luna en regencia. 2. verifica como le afecta a tu luna natal. 3. cuida de tus emociones y combina ambas lunas esos dos ias y medio para poder darte de la mejor manera lo que necesitas');
+INSERT INTO moon (sign, descripcion, afectacion)
+
+VALUES ('Geminis', 'no le cuesta encontrar palabras para hablar de sus sentimientos y, de hecho, es bastante impulsivo a la hora de hacerlo. En el amor, buscan a alguien con quien compartir un vínculo profundo.', '1. comprueba en que signos esta la luna en regencia. 2.verifica como le afecta a tu luna natal. 3. cuida de tus emociones y combina ambas lunas esos dos ias y medio para poder darte de la mejor manera lo que necesitas');
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2/promise");
 
-// require('dotenv').config();
+require('dotenv').config();
 
 const server = express();
 server.use(cors());
@@ -17,14 +17,60 @@ server.listen(PORT, ()=>{
 
 async function getConnection () {
     const conex = await mysql.createConnection({
-        host:"localhost",
-        user:"root",
-        password:"Izan2018!",
-        database:"moon",
+        host: process.env.DB_HOST || "localhost",
+        user:process.env.DB_USER || "root",
+        password:process.env.DB_PASS || "Izan2018!",
+        database:process.env.DB_NAME || "moon",
     });
 
     await conex.connect();
     console.log('conexion con la BD' + conex.threadId);
     return conex;
 }
-getConnection();
+// getConnection();
+
+//endpoint busca todas las lunas
+
+server.get('/moon', async(req, res)=>{
+
+    try {
+      
+        const conn = await getConnection();
+        const select = 'SELECT * FROM moon';
+        const [result] = await conn.query(select);
+        if (result.length === 0) {
+            res.status(400),json({ message: 'El id no existe en la BD'});
+
+        } else {
+            res.status(200).json({ data: result });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error);
+        
+    }
+});
+
+//endpoint por id (campo de mi interés)
+
+server.get('/moon/:id', async(req, res)=>{
+
+    try {
+        // const id = req.params.id;
+        const { id } = req.params;
+        const conn = await getConnection();
+        const select = 'select * from moon where id = ?';
+        const [result] = await conn.query(select, [id]);
+        //hago validaciones
+        if (result.length === 0) {
+            res.status(400),json({ message: 'El id no existe en la BD'});
+
+        } else {
+            res.status(200).json({ data: result[0] });
+        }
+    } catch (error) {
+        // console.log(error);
+        res.status(400).json(error);
+        
+    }
+});
